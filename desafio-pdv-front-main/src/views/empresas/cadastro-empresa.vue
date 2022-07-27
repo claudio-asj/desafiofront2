05b48e92ae2862ac7c86f20280dbb2d2a223b7e5
Claudio Junior <cla.junior21@gmail.com>
2022-07-27 10:32:55 -0300

    criei pagina de cadastro-empresa

--- desafio-pdv-front-main/src/views/empresas/cadastro-empresa.vue
+++ desafio-pdv-front-main/src/views/empresas/cadastro-empresa.vue
@@ -1,3 +1,36 @@
 <template>
-  
+    <div>
+    <Layout />
+    <div class="page-container">
+      <PageHeader
+        title="Cadastro de Empresa"
+        helpTitle="Cadastro de Empresa"
+        :helpText="helpText"
+      />
+      <div class="my-container">
+        <b-row>
+            <b-col>
+                <b-card>a</b-card>
+            </b-col>
+            <b-col>
+                <b-card>b</b-card>
+            </b-col>
+        </b-row>
+    </div>
+  </div>
+    </div>
 </template>
+
+<script>
+import PageHeader from "@/components/page-header";
+import StateButtonBar from "@/components/state-button-bar";
+import Layout from "@/layout/main";
+
+export default {
+  components: {
+    PageHeader,
+    StateButtonBar,
+    Layout,
+  }
+}
+</script>
